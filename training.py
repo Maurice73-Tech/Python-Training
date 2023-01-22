d044a05bcc1892ce984de6c564f3a53aa85699f0
maurice neidhart <maurice.neidhart97@gmail.com>
2023-01-22 15:14:15 +0100

    first class with method

--- training.py
+++ training.py
@@ -1,2 +1,13 @@
 # Hello Training print out
 print('Let the training begin')
+
+# Write a class with a constructor and a method
+class Training:
+    def __init__(self, name):
+        self.name = name
+    def print_name(self):
+        print(self.name)
+
+# Call the method print_name and print the name 'Thomas'
+training = Training('Thomas')
+training.print_name()
